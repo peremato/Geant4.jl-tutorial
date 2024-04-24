9224122249fcb07c9453345e08b8ec2b2158a027
Pere Mato <pere.mato@cern.ch>
2024-04-24 10:27:26 +0200

    Support theadid == -2

--- tutorial/docs/examples/Scintillation/UserActions.jl
+++ tutorial/docs/examples/Scintillation/UserActions.jl
@@ -92,7 +92,7 @@ end
 #---End Run Action---------------------------------------------------------------------------------
 function endrun(run::G4Run, app::G4JLApplication)::Nothing
     #---end run action is called for each workwer thread and the master one
-    if G4Threading!G4GetThreadId() == -1   
+    if G4Threading!G4GetThreadId() < 0   
         data = app.simdata[1]
         #---This is the master thread, so we need to add all the simuation results-----------------
         for d in app.simdata[2:end]
